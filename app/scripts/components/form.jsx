ba0f505b5c0beeccc3497e2067fbe51a721c53ad
dylan-gregory <jdylangregory@gmail.com>
2017-02-20 21:41:10 -0500

    Attempted deleting and editing to no avail.

--- app/scripts/components/form.jsx
+++ app/scripts/components/form.jsx
@@ -10,8 +10,14 @@ var PhotoAddForm = React.createClass({
     event.preventDefault();
     console.log("clicked");
     this.props.addImage(this.state);
+    this.state.photos.fetch().done(function(){
+      self.forceUpdate();
+    });
 
   },
+  // deletePost: function(){
+  //   ReactDOM.unmountComponentAtNode(this.state.photos);
+  // },
   handleUrl: function(event){
     this.setState({url: event.target.value});
   },
@@ -24,16 +30,15 @@ var PhotoAddForm = React.createClass({
   render: function(){
 
     return (
-      <div>
-        <form onSubmit={this.addImage}>
+
+        <form onSubmit={this.addImage} >
           <div className="form-group">
             <input type="text" className="form-control new-img-url" placeholder="New Image URL" value={this.state.url} onChange={this.handleUrl}/>
             <input type="text" className="form-control new-title" placeholder="New Image Title" value={this.state.title} onChange={this.handleTitle} />
             <input type="text" className="form-control new-caption" placeholder="New Image Caption" value={this.state.caption} onChange={this.handleCaption}/>
-            <button className="btn btn-primary submit" type="button">Submit</button>
+            <button className="btn btn-primary submit" type="submit">Submit</button>
           </div>
         </form>
-      </div>
     )
   }
 });
--- app/scripts/components/listing.jsx
+++ app/scripts/components/listing.jsx
@@ -20,6 +20,9 @@ var ImageBoard = React.createClass({
     images.create(photo);
     this.setState({photos: images});
   },
+  deletePost: function(){
+    ReactDOM.unmountComponentAtNode(this.state.photos);
+  },
   render: function(){
 
     return (
@@ -36,21 +39,25 @@ var ImageList = React.createClass({
   isEditing: function(){
 
   },
+  // deletePost: function(){
+  //   ReactDOM.unmountComponentAtNode(this.state.photos);
+  // },
   render: function(){
     var listedPhotos = this.props.photos.map(function(photo){
 
       return (
-          <div className="col-sm-6 col-md-4">
+          <div className="col-sm-6 col-md-4" key={photo.cid}>
             <div className="thumbnail">
               <img src={photo.get('url')} />
               <div className="caption">
                 <h3>{photo.get('title')}</h3>
                 <p>{photo.get('caption')}</p>
-                <p><a href="#" onClick={this.props.isEditing} className="btn btn-primary" role="button">Edit</a></p>
+                <p><a href="#" className="btn btn-primary" role="button">Edit</a><a href="#" className="btn btn-primary" role="button">Delete</a></p>
               </div>
             </div>
           </div>
       )
+
 
     });
 
